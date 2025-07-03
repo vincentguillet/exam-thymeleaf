e82cbb14cabf14cfa2ebd8b745d9771cc3bc99d9
Vincent Guillet <vincent.guillet@outlook.com>
2025-07-03 09:39:49 +0200

    first commit

---
+++ src/main/java/com/humanbooster/ExamThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.humanbooster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamThymeleafApplication.class, args);
+	}
+
+}
