d5aba7913871b19022463b401d6d3c68725c8167
Vincent Guillet <vincent.guillet@outlook.com>
2025-07-03 10:28:53 +0200

    add DTO classes for Project, Task, and User

---
+++ src/main/java/com/humanbooster/dto/ProjectDTO.java
@@ -0,0 +1,19 @@
+package com.humanbooster.dto;
+
+import com.humanbooster.model.Task;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectDTO {
+    private Long id;
+    private String name;
+    private String creator;
+    private List<Task> tasks = new ArrayList<>();
+}
---
+++ src/main/java/com/humanbooster/dto/TaskDTO.java
@@ -0,0 +1,15 @@
+package com.humanbooster.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskDTO {
+    private Long id;
+    private String title;
+    private String status;
+    private String assignee;
+}
